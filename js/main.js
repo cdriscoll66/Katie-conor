6f89f8e301a18b907d7426db29f97a0006b87843
Conor Driscoll <conor@driscoll66.com>
2017-01-02 16:58:28 +0100

    This is background sunset and stars
    background sunset and stars with triggers.

--- js/main.js
+++ js/main.js
@@ -1,57 +1,92 @@
 // JavaScript Document
 
-
+/*background colors*/
 
 $(function() {
     // Init Controller
 	
     var scrollMagicController = new ScrollMagic();
 
-	  var tween = TweenMax.to('body', 6, {backgroundColor:"#000"});
-
-	// Create the Scene and trigger when visible with ScrollMagic
-var scene = new ScrollScene({
-    triggerElement: '#scene',
-duration: 300
+	  var tween1 = TweenMax.to('.container', 6, {backgroundColor:"rgba(253,165,0,.3)"});
+	var scene1 = new ScrollScene({
+    triggerElement: '#scene1',
+duration: 10000
 })
-	
-
- 
-.setTween(tween)
+.setClassToggle('.container', 'scene-1-active')
+.setTween(tween1)
 .addTo(scrollMagicController)
-   
 	
-scene.addIndicators();
+	  var tween2 = TweenMax.to('.container', 6, {backgroundColor:"rgba(247,3,3,0.3)"});
+var scene2 = new ScrollScene({
+    triggerElement: '#scene2',
+duration: 5000
+})
+.setClassToggle('.container', 'scene-2-active')
+.setTween(tween2)
+.addTo(scrollMagicController)
+
+	  var tween3 = TweenMax.to('.container', 6, {backgroundColor:"rgba(28,3,42,0.94)"});
+var scene3 = new ScrollScene({
+    triggerElement: '#scene3',
+duration: 5000
+})
+.setClassToggle('.container', 'scene-3-active')
+.setTween(tween3)
+.addTo(scrollMagicController)   
+
+	  var tween4 = TweenMax.to('.container', 6, {backgroundColor:"rgba(28,3,42,0.7)"});
+var scene4 = new ScrollScene({
+    triggerElement: '#scene4',
+duration: 20000
+})
+.setClassToggle('.container', 'scene-3-active')
+.setTween(tween4)
+.addTo(scrollMagicController)
 	
-	
-	
+scene1.addIndicators();
+scene2.addIndicators();
+scene3.addIndicators();
+scene4.addIndicators();
 
 });
-/*
 
-// When the DOM is ready
-$(function() {
-  
-  // Init ScrollMagic Controller
-  var scrollMagicController = new ScrollMagic();
-  
-  // Create Animation for 0.5s
-  var tween = TweenMax.to('body', 5, {
-    backgroundColor: 'rgb(255, 39, 46)'
-  });
-  
+/*stars
 
-  
-  // Create the Scene and trigger when visible
-  var scene = new ScrollScene({
-    triggerElement: '#scene',
-    offset: 150 
-  })
-  .setTween(tween)
-  .addTo(scrollMagicController);
-  
-  // Add debug indicators fixed on right side
-   scene.addIndicators();
-  
-});
+TweenMax.to('#stars1', 10, {opacity:1});
+TweenMax.to('#stars2', 25, {opacity:1});
+TweenMax.to('#stars3', 41, {opacity:1});
+
 */
+
+$(function() {
+    // Init Controller
+    var scrollMagicController = new ScrollMagic();
+	
+	var tween1 = TweenMax.to('#stars1', 26, {opacity:1});
+	
+	var scene1 = new ScrollScene({
+    triggerElement: '#scene2',
+    offset: 0 /* offset the trigger 150px below #scene's top */
+})
+.setTween(tween1)
+.addTo(scrollMagicController);
+	
+	var tween2 = TweenMax.to('#stars2', 25, {opacity:1});
+	
+	var scene2 = new ScrollScene({
+    triggerElement: '#scene3',
+    offset: 0 /* offset the trigger 150px below #scene's top */
+})
+.setTween(tween2)
+.addTo(scrollMagicController);
+	
+		var tween3 = TweenMax.to('#stars3', 26, {opacity:1});
+	
+	var scene3 = new ScrollScene({
+    triggerElement: '#scene4',
+    offset: 0 /* offset the trigger 150px below #scene's top */
+})
+.setTween(tween3)
+.addTo(scrollMagicController);
+
+});
